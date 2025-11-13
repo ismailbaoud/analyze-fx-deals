f310f5de5b34ad4a402a1b258918850d38d628db
ismail baoud <ibaoud46@egmail.com>
2025-11-13 23:34:36 +0100

    feat: add input validation to RequestDTO
    - Validate deal ID length (1-50 characters)
    - Enforce 3-letter ISO currency codes
    - Ensure non-null timestamp and positive deal amount
    - Provide descriptive validation error messages

--- src/main/java/com/progresssoft/analyze_fx_deals/dto/RequestDTO.java
+++ src/main/java/com/progresssoft/analyze_fx_deals/dto/RequestDTO.java
@@ -2,7 +2,7 @@ package com.progresssoft.analyze_fx_deals.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +11,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestDTO {
+    @NotBlank(message = "Deal ID cannot be blank")
+    @Size(min = 1 , max=50 , message = "Deal ID must be between 1 and 50 characters")
+    private String dealUniqueId;
+
+    @NotBlank(message = "From currency cannot be blank")
+    @Size(min = 3 ,max = 3 ,message = "Currency code must be exactly 3 letters")
     private String fromCurrencyIsoCode;
+
+    @NotBlank(message = "To currency cannot be blank")
+    @Size(min = 3 ,max = 3 ,message = "Currency code must be exactly 3 letters")
     private String toCurrencyIsoCode;
+
+    @NotNull(message = "Deal timestamp cannot be null")
     private LocalDateTime dealTimestamp;
+
+    @NotBlank(message = "Deal amount cannot be null")
+    @DecimalMin(value = "0.01", message = "Deal amount must be greater than 0")
     private BigDecimal dealAmount;
 }
