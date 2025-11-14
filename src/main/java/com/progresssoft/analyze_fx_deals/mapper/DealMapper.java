f5042977dca6d289926189132545d942581be539
ismail baoud <ibaoud46@egmail.com>
2025-11-15 00:47:34 +0100

    feat(mapper): add MapStruct mapper interface
    - Create DealMapper interface for DTO-Entity conversion
    - Configure MapStruct for bidirectional mapping
    - Add toEntity and toDto methods

--- src/main/java/com/progresssoft/analyze_fx_deals/mapper/DealMapper.java
+++ src/main/java/com/progresssoft/analyze_fx_deals/mapper/DealMapper.java
@@ -2,8 +2,6 @@ package com.progresssoft.analyze_fx_deals.mapper;
 
 
 import org.mapstruct.Mapper;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 
 import com.progresssoft.analyze_fx_deals.dto.RequestDTO;
 import com.progresssoft.analyze_fx_deals.model.Deal;
@@ -12,10 +10,4 @@ import com.progresssoft.analyze_fx_deals.model.Deal;
 public interface DealMapper {
     RequestDTO toDto(Deal d);
     Deal toEntity(RequestDTO rDto);
-
-    default void validate(RequestDTO rDto
-    ) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validate'");
-    }
 }
